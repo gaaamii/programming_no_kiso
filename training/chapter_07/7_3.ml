e444a61af362a410d6438c10c2e90619fb7c33fd
gaaamii <i1shi4ga4mi1@gmail.com>
2015-08-13 14:49:29 +0900

    y軸について対象になってしまっていたのでx軸について対象になるように修正

--- training/chapter_07/7_3.ml
+++ training/chapter_07/7_3.ml
@@ -1,9 +1,9 @@
 (* 目的：x座標とy座標の組で表された平面座標を受け取ったら,x軸について対象な点の座標を返す *)
 (* taisho_x : int * int -> int * int *)
 let taisho_x coordinate = match coordinate with
-  (x, y) -> (-x, y)
+  (x, y) -> (x, -y)
 
 (* テスト *)
-let test1 = taisho_x (1, 2) = (-1, 2)
-let test2 = taisho_x (2, 2) = (-2, 2)
-let test3 = taisho_x (3, 2) = (-3, 2)
+let test1 = taisho_x (1, 1) = (1, -1)
+let test2 = taisho_x (1, 2) = (1, -2)
+let test3 = taisho_x (1, 3) = (1, -3)
