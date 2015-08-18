03a08d851175697d9a610b381b50bdc8ab58d809
gaaamii <i1shi4ga4mi1@gmail.com>
2015-08-19 00:16:11 +0900

    9.1, 9.2 完了

---
+++ training/chapter_09/9_2.ml
@@ -0,0 +1,9 @@
+#use "../chapter_08/8_3.ml"
+
+let people =
+  { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }
+  :: { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }
+  :: { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }
+  :: { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }
+  :: { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }
+  :: []
