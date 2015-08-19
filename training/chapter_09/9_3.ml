260508c58ab5a60f1ffef431aac404d07c4dd6b9
gaaamii <i1shi4ga4mi1@gmail.com>
2015-08-19 22:32:10 +0900

    9.3 完了

---
+++ training/chapter_09/9_3.ml
@@ -0,0 +1,10 @@
+#use "../chapter_08/8_3.ml"
+
+let shiki = ["春"; "夏"; "秋"; "冬"]
+let people = [
+  { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; };
+  { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }; 
+  { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; };
+  { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; };
+  { name = "田中"; height = 190; weight = 80; birthday = "02/04"; blood = "A"; }
+]
